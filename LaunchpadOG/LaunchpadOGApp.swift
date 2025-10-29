cf3b3ad61bd389325c7e73b18ca72bfdfc25fcf5
Kot <86029286+SKbarbon@users.noreply.github.com>
2025-10-29 08:15:43 +0300

    Initial Commit

---
+++ LaunchpadOG/LaunchpadOGApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchpadOGApp.swift
+//  LaunchpadOG
+//
+//  Created by Yousef on 29/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct LaunchpadOGApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
